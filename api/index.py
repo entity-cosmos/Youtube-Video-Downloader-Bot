a74b453df0834bf920f72854d21974b9c457eb60
Srajan B Shetty <srajanbshetty@gmail.com>
2025-02-06 17:49:54 +0530

    Refactor Telegram bot integration to use Dispatcher and simplify webhook handling

--- api/index.py
+++ api/index.py
@@ -4,10 +4,8 @@ from typing import Optional
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes, filters
-from pytube import YouTube
-import os
+from telegram import Update, Bot
+from telegram.ext import Dispatcher, MessageHandler, Filters, CommandHandler
 
 TOKEN = os.environ.get("TOKEN")
 
@@ -30,81 +28,34 @@ class TelegramWebhook(BaseModel):
     poll: Optional[dict]
     poll_answer: Optional[dict]
 
-class YoutubeDownloaderBot:
-    def __init__(self):
-        self.downloaded_videos = {}
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-    def download_video(self, video_url):
-        try:
-            youtube = YouTube(video_url)
-            video = youtube.streams.get_highest_resolution()
-            file_path = video.download()
-            self.downloaded_videos[video.title] = file_path
-            return f"Video '{video.title}' downloaded successfully.", file_path
-        except Exception as e:
-            return f"Error: {str(e)}", None
-
-    def get_downloaded_videos(self):
-        return list(self.downloaded_videos.keys())
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    text = (
-        "Welcome To Cosmos YT Videos Downloader.\n"
-        "Here are the commands you can use:\n"
-        "/start - Start the bot\n"
-        "/yt <youtubeVideoUrl> - Download a YouTube video\n"
-        "/list - List downloaded videos\n"
-        "Only Small Videos are supported for now.\n"
-        "Made with ❤️ by @Srajan_B_Shetty\n"
-    )
-    await update.message.reply_text(text)
-
-async def download_video(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    try:
-        bot = context.bot
-        args = context.args
-        video_url = " ".join(args)
-
-        if not video_url:
-            await update.message.reply_text("Please provide a YouTube video URL.")
-            return
-
-        response, file_path = youtube_downloader_bot.download_video(video_url)
-        await update.message.reply_text(response)
-        if file_path:
-            with open(file_path, 'rb') as video_file:
-                await bot.send_video(chat_id=update.message.chat_id, video=video_file)
-        #delete the video after sending it on telegram
-        os.remove(file_path)
-    except Exception as e:
-        os.remove(file_path)
-        await update.message.reply_text(f"Error: {str(e)}")
-
-
-async def list_downloaded_videos(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    downloaded_videos = youtube_downloader_bot.get_downloaded_videos()
-    if downloaded_videos:
-        text = "Downloaded Videos:\n" + "\n".join(downloaded_videos)
-    else:
-        text = "No videos downloaded yet."
-    await update.message.reply_text(text)
-
-app = FastAPI()
+def register_handlers(dispatcher):
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
 @app.post("/webhook")
 def webhook(webhook_data: TelegramWebhook):
-    global youtube_downloader_bot
-    youtube_downloader_bot = YoutubeDownloaderBot()
+    '''
+    Telegram Webhook
+    '''
+    # Method 1
+    bot = Bot(token=TOKEN)
+    update = Update.de_json(webhook_data.__dict__, bot) # convert the Telegram Webhook class to dictionary using __dict__ dunder method
+    dispatcher = Dispatcher(bot, None, workers=4)
+    register_handlers(dispatcher)
 
-    application = Application.builder().token("5385502733:AAEkgNIbNL9sTyguk---nDiS0c-JMX2rybw").build()
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("yt", download_video))
-    application.add_handler(CommandHandler("list", list_downloaded_videos))
+    # handle webhook request
+    dispatcher.process_update(update)
 
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-    return {"message": "Webhook received successfully"}
+    # Method 2
+    # you can just handle the webhook request here without using python-telegram-bot
+    # if webhook_data.message:
+    #     if webhook_data.message.text == '/start':
+    #         send_message(webhook_data.message.chat.id, 'Hello World')
 
-
+    return {"message": "ok"}
 
 @app.get("/")
 def index():
